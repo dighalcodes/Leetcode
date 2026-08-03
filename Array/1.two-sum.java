212a760e6cab1557ea08032c7ce3a0bc43788f70
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-03 17:00:39 +0530

    added 2sum problem

---
+++ Array/1.two-sum.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+import java.util.*;
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { 
+                    numMap.get(complement), i 
+                };
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
+// @lc code=end
+
