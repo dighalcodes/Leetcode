46c024d206b4df276d6db7c2c6dfe131493ddd2e
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-05 20:37:22 +0530

    rotate array(189) is added

---
+++ 01_Array/04__189.rotate-array.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 0) {
+            return;
+        }
+        int n = nums.length;
+        k = k % n;
+        if (k == 0) {
+            return;
+        }
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+        for (int i = n - 1; i >= k; i--) {
+            nums[i] = nums[i-k];
+        }
+        for (int i = 0; i < temp.length; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
