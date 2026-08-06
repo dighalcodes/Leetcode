7739de2b71423bd0d6cc5a33ed9c2da2b9566360
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-06 15:59:59 +0530

    move zeroes(283) added

---
+++ 01_Array/05__283.move-zeroes.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        int p = 0;
+        int n = nums.length;
+
+        if (nums.length == 0) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        for (int i = n - 1; i >=j; i--) {
+            nums[i] =0;
+        }
+    }
+}
+// @lc code=end
