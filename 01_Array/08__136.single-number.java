93cb7904b01c98b05599153e037e87222e508a7b
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-09 19:02:43 +0530

    problem- sort-colors(75) is added

--- 01_Array/08__136.single-number.java
+++ 01_Array/08__136.single-number.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 class Solution {
     public int singleNumber(int[] nums) {
-         Arrays.sort(nums);
+        Arrays.sort(nums);
         int result =0;
         for(int i = 0; i<nums.length; i++){
             result = result^nums[i];
---
+++ 01_Array/09__75.sort-colors.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+class Solution {
+    private void swap(int[] nums, int i , int j){
+        int temp = nums[i];
+        nums[i]= nums[j];
+        nums[j] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int low = 0, mid= 0, high = nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+}
+// @lc code=end
+
