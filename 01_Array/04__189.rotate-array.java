5193375255206cd561bef4a1d4790fe7e5003983
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-16 22:48:03 +0530

    problem 189 rotate array is modified

--- 01_Array/04__189.rotate-array.java
+++ 01_Array/04__189.rotate-array.java
@@ -1,24 +1,29 @@
 
 class Solution {
     public void rotate(int[] nums, int k) {
-        if (nums.length == 0) {
+        if (nums == null || nums.length <= 1) {
             return;
         }
+
         int n = nums.length;
-        k = k % n;
+        k = k % n; 
+
         if (k == 0) {
             return;
         }
-        int[] temp = new int[k];
 
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];
-        }
-        for (int i = n - 1; i >= k; i--) {
-            nums[i] = nums[i-k];
-        }
-        for (int i = 0; i < temp.length; i++) {
-            nums[i] = temp[i];
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
