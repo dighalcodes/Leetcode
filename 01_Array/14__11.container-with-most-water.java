d2b38cbcc5607f59b40149bb9ca91f20bd39a616
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-17 19:42:48 +0530

    problem-11 is added

--- 01_Array/14__11.container-with-most-water.java
+++ 01_Array/14__11.container-with-most-water.java
@@ -1,14 +1,22 @@
-/*
- * @lc app=leetcode id=11 lang=java
- *
- * [11] Container With Most Water
- */
 
-// @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-        
+        int left = 0;
+        int right = height.length - 1;
+        int maxi = 0;
+        while (left < right) {
+            int currHeight = Math.min(height[left], height[right]);
+            int currWidth = right - left;
+            int area = currHeight*currWidth;
+            maxi = Math.max(max, area);
+
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxi;
     }
 }
-// @lc code=end
 
