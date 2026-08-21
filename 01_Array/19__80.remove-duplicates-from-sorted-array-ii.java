a33d1c8d5fae4a0dd4722043687838da51883bf2
Dighal Saha <dighal.saha.dev@gmail.com>
2026-08-21 18:49:12 +0530

    problem-80 added

---
+++ 01_Array/19__80.remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=80 lang=java
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ */
+
+// @lc code=startE
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+        int slow = 2;
+        for (int fast = 2; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow - 2]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
+// @lc code=end
